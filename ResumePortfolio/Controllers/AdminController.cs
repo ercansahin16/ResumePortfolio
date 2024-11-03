e3682e90c6f7fdeb1fe2aae923cf1e6bbbb88f59
Ercan sahin <esahin.ast@gmail.com>
2024-11-03 17:29:41 +0300

    Day

--- ResumePortfolio/Controllers/AdminController.cs
+++ ResumePortfolio/Controllers/AdminController.cs
@@ -23,7 +23,9 @@ namespace ResumePortfolio.Controllers
        var user= _context.logins.FirstOrDefault(x=>x.Email==login.Email && x.password==login.password);
          if(user!=null)
          {
+          
             return RedirectToAction("Index");
+            
          }
          {
             TempData["ErrorMessage"] = "Geçersiz kullanıcı adı veya şifre.";
@@ -35,6 +37,23 @@ namespace ResumePortfolio.Controllers
       {
         var values= _context.blogPosts.ToList();
          return View(values);
+      }
+      public IActionResult BlogPostUpdate(int id)
+      {
+         var values = _context.blogPosts.Find(id);
+         return View(values);
+      }
+      [HttpPost]
+      public IActionResult BlogPostUpdate(BlogPosts blogPosts)
+      {
+         var img = "/tema/assets/img/mediüm/" + blogPosts.Img;
+         blogPosts.Img = img;
+         var team = "/tema/assets/img/team/" + blogPosts.TeamImg;
+         blogPosts.TeamImg = team;
+
+         _context.blogPosts.Update(blogPosts);
+         _context.SaveChanges();
+         return RedirectToAction("BlogPost");
       }
    }
 }
